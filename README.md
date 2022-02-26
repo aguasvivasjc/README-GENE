aa6762c3bde5f4d0e2f93ce7c56ae737009d62d4
Jancarlos Aguasvivas <aguasvivasjc@gmail.com>
2022-02-26 15:06:17 -0500

    Generator Ready

--- README.md
+++ README.md
@@ -1,14 +1,43 @@
-## README Generator 
+ README-GENE
 
-From the terminal create a README.md application that will be generated solely through a terminal.
-
-The purpose of having a README.md file is to create detailed documentation of your project. 
-This is useful not only to you but to whom ever may use your application. 
+![GitHub](https://img.shields.io/github/license/aguasvivasjc/README-GENE)
 
 
-## Executed via terminal 
+# Description
 
+This application will generate a README.md file through the terminal
 
+# Table of Contents
 
+* [License](#license)
+* [User Story](#user-story)
+* [Contribution Guidelines](#con)
+* [Test Instructions](#test)
+* [Questions](#email)
 
-## GitHub Link
+# License
+
+MIT License
+
+# User Story
+
+The usage for this is to reduce time spent on creating a README file. The time spend on this can be automated and developers can spend more time working on function code and/or design. 
+
+# Contribution Guidelines
+
+This is an open source application. 
+
+# Test Instructions 
+
+To test this application use "node index.js" and the prompts for the README.md file will appear. 
+
+# Questions
+
+## GitHub User Name 
+
+* aguasvivasjc
+
+## Email Address
+
+* aguasvivasjc@gmail.com
+
--- index.js
+++ index.js
@@ -1,70 +1,131 @@
 const inquirer = require('inquirer');
 const fs = require('fs');
+const path = require('path');
 
 const askUser = () => {
-inquirer.prompt([
+    inquirer.prompt([
 
-    {
-        type: "input",
-        name: "title",
-        message: "what is the title of your project?",
-    },
+        {
+            type: "input",
+            name: "title",
+            message: "what is the title of your project?",
+        },
 
-    {
-        type: "input",
-        name: "description",
-        message: "Please provide a project description",
-    },
+        {
+            type: "input",
+            name: "description",
+            message: "Please provide a project description",
+        },
 
-    {
-        type: "input",
-        name: "license",
-        message: "Do you want to add a license?",
-    },
+        {
+            type: "list",
+            name: "license",
+            message: "Do you want to add a license?",
+            choices: [
+                "None",
+                "Apache License 2.0",
+                "GNU General Public License v3.0",
+                "MIT License",
+                "Boost Software License 1.0",
+                "Eclipse Public License 2.0",
+            ]
+        },
 
-    {
-        type: "input",
-        name: "user",
-        message: "What is your User Story?",
-    },
+        {
+            type: "input",
+            name: "user",
+            message: "What is your User Story?",
+        },
 
-    {
-        type: "input",
-        name: "github",
-        message: "What is your GitHub username?",
-    },
+        {
+            type: "input",
+            name: "con",
+            message: "What are your contribution guidelines?",
+        },
 
-     {
-        type: "input",
-        name: "Repo",
-        message: "What is your repo name?",
-    },
-])
+        {
+            type: "input",
+            name: "test",
+            message: "What are your test instructions?",
+        },
 
+        {
+            type: "input",
+            name: "github",
+            message: "What is your GitHub username?",
+        },
 
-    .then(({ title, description, license, user, github }) => {
-        fs.writeFile('README.md'
+        {
+            type: "input",
+            name: "repo",
+            message: "What is your repo name?",
+        },
+
+        {
+            type: "input",
+            name: "email",
+            message: "What is your email address?",
+        },
+
+    ])
+        //     .then(({ title, description, license, user, github, repo }) => {
+        .then((data) => {
+            writeToFile('README.md', template({ ...data }))
+
+        });
+}
+//             fs.writeFile('README.md',
 
 //  README Template 
 
-` ${title}
+const template = (data) => {
+    return ` ${data.title}
 
-![GitHub](https://img.shields.io/github/license/${github}/${repo})
+![GitHub](https://img.shields.io/github/license/${data.github}/${data.repo})
 
 
-## Description
+# Description
 
-${description}
+${data.description}
 
-## License
+# Table of Contents
 
-${license}
+* [License](#license)
+* [User Story](#user-story)
+* [Contribution Guidelines](#con)
+* [Test Instructions](#test)
+* [Questions](#email)
 
-## User Sotory 
+# License
 
-${user}
+${data.license}
 
-## Deployed Link  
-# Deployed link here: 
+# User Story
 
-`
+${data.user}
+
+# Contribution Guidelines
+
+${data.con}
+
+# Test Instructions 
+
+${data.test}
+
+# Questions
+
+## GitHub User Name 
+
+* ${data.github}
+
+## Email Address
+
+* ${data.email}
+
+`}
+
+const writeToFile = (fileName, data) => {
+    return fs.writeFileSync(path.join(process.cwd(), fileName), data)
+}
+
+askUser();
